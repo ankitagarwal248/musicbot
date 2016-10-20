2127c4e751c840866970f03a4042f602f5bad900
Ankit Agarwal <ankitagarwal24.8@gmail.com>
2016-10-21 03:20:08 +0530

    minor

--- bookworm_project/settings_base.py
+++ bookworm_project/settings_base.py
@@ -113,8 +113,6 @@ STATICFILES_DIRS = (
     # os.path.join(PROJECT_ROOT, 'static'),
 )
 
-
-
 SENDER_EMAIL_ID = "Ankit Agarwal <ankitagarwal24.8@gmail.com>"
 
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
@@ -122,8 +120,7 @@ MAILGUN_KEY = "key-ee56db9c07e16b20bb30954e93ff5f1d"
 REQUEST_URL = "https://api.mailgun.net/v3/tubebuses.com/messages"
 MAILGUN_ACCESS_KEY = MAILGUN_KEY
 MAILGUN_SERVER_NAME = 'tubebuses.com'
-EMAIL_SUBJECT_PREFIX = "[Tube : Environment] "
-
+EMAIL_SUBJECT_PREFIX = "[Bookworm : Environment] "
 
 ADMINS = (
     ('Ankit Agarwal', 'ankitagarwal24.8@gmail.com')
