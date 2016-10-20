55fb45579f93eb05a82441e2cf76a0e413e78aa7
Ankit Agarwal <ankitagarwal24.8@gmail.com>
2016-10-21 01:58:40 +0530

    minor

--- bookworm_project/settings_base.py
+++ bookworm_project/settings_base.py
@@ -104,10 +104,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 SENDER_EMAIL_ID = "Ankit Agarwal <ankitagarwal24.8@gmail.com>"
